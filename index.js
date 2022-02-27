d22cacd8553ccd573189dcddad51bb090a7d118b
LisaUhl02 <7061@htl.rennweg.at>
2022-02-27 14:02:00 +0100

    v2_new_chart

--- index.js
+++ index.js
@@ -5,26 +5,17 @@ Highcharts.chart('container', {
     title: {
         text: 'Aufwand pro Medium'
     },
-    accessibility: {
-        announceNewData: {
-            enabled: true
-        }
-    },
     xAxis: {
-        type: 'category',
-
-        title: {
-            text: 'Medium'
-        }
+        categories: ['Podcast', 'Fotoreihe', 'Nachrichtensendung', 'Mockumentary', 'Comic', 'Stop-Motion']
     },
     yAxis: {
         title: {
             text: 'Stunden'
         }
-
     },
-    legend: {
-        enabled: false
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span> <b>{point.y:.2f} Stunden</b> insgesamt<br/>'
     },
     plotOptions: {
         series: {
@@ -36,49 +27,22 @@ Highcharts.chart('container', {
             }
         }
     },
-
-    tooltip: {
-        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f} Stunden</b> insgesamt<br/>'
-    },
-
-    series: [
-        {
-            name: "Medium",
-            colorByPoint: true,
-            data: [
-
-                {
-                    name: "Podcast",
-                    y: 8,
-                    drilldown: "Podcast"
-                },
-                {
-                    name: "Fotoreihe",
-                    y: 9.5,
-                    drilldown: "Fotoreihe",
-                },
-                {
-                    name: "Nachrichtensendung",
-                    y: 14.2,
-                    drilldown: "Nachrichtensendung"
-                },
-                {
-                    name: "Mockumentary",
-                    y: 35.72,
-                    drilldown: "Mockumentary"
-                },
-                {
-                    name: "Comic",
-                    y: 40.8,
-                    drilldown: "Comic"
-                },
-                {
-                    name: "Stop-Motion",
-                    y: 41.2,
-                    drilldown: "Stop-Motion"
-                }
-            ]
-        }
+    series: [{
+        name: 'Vorproduktion',
+        colorByPoint: true,
+        data: [1, 2, 2, 4, 3, 2]
+    }, {
+        name: 'Produktion',
+        colorByPoint: true,
+        data: [3, 3, 4, 15.2, 18, 20]
+    }, {
+        name: 'Nachbearbeitung',
+        colorByPoint: true,
+        data: [4, 4.5, 8.2, 16.52, 19.5, 19.2]
+    }, {
+        name: 'Gesamtstunden',
+        colorByPoint: true,
+        data: [8, 9.5, 14.2, 35.72, 40.5, 41.2]
+    }
     ]
-})
+});
